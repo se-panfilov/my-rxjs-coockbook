b8c0ae32cbe7ef1ce4efa0f24dbab8038ea3f763
Sergei Panfilov <se-panfilov@ya.ru>
2019-02-14 13:53:47 +0100

    issue for async http calls

---
+++ examples/cancel-prev-requests.js
@@ -0,0 +1,31 @@
+const {of, Subject} = require('rxjs');
+const {delay, tap, switchMap, flatMap} = require('rxjs/operators')
+
+//ignore undefined
+
+const userInputSubj = new Subject();
+
+// Subscribe for user input
+userInputSubj
+  .pipe(
+    tap(v => console.log('user input: ', v)), // output user's input
+    // flatMap(v => makeQuery(v)) // this will produce request in the same order as input was done
+    switchMap(v => makeQuery(v)) // this will make request only for last input
+  )
+  .subscribe(val => {
+    console.log('request:', val)
+  })
+
+// User add some values in "runtime"
+setTimeout(() => userInputSubj.next('John'), 800)
+setTimeout(() => userInputSubj.next('John "Bones"'), 1600)
+setTimeout(() => userInputSubj.next('John "Bones" Johns'), 2400)
+
+
+//Simulate http request here
+
+
+function makeQuery(value) {
+  const due = (value === 'John') ? 3000 : 880
+  return of(value).pipe(delay(due))
+}
--- examples/ignore-undefined.js
+++ examples/ignore-undefined.js
@@ -1,11 +1,12 @@
 const {of} = require('rxjs');
-const {filter} = require('rxjs/operators')
+const {filter, tap} = require('rxjs/operators')
 
 //ignore undefined
 
 of(...[1, 2, 3, undefined, 5])
   .pipe(
     filter((v) => v !== undefined)
+    // tap(v => console.log('tap: ', v))
   )
   .subscribe(res => {
     console.log('result: ', res)
