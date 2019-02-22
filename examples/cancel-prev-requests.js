022ed52b0f23ff53fab1f975a28715c5ae193188
Sergei Panfilov <se-panfilov@ya.ru>
2019-02-22 15:20:36 +0100

    cancell prev requests

--- examples/cancel-prev-requests.js
+++ examples/cancel-prev-requests.js
@@ -1,31 +1,32 @@
-const {of, Subject} = require('rxjs');
-const {delay, tap, switchMap, flatMap} = require('rxjs/operators')
+const {of, Subject} = require('rxjs')
+const {delay, takeUntil} = require('rxjs/operators')
 
-//ignore undefined
+// Case:
+// Wend several http calls in an order, one after another one
+// But the last one call executes ended first, and a call from the middle - ended last
 
-const userInputSubj = new Subject();
+const cancelRequest = new Subject();
 
-// Subscribe for user input
-userInputSubj
-  .pipe(
-    tap(v => console.log('user input: ', v)), // output user's input
-    // flatMap(v => makeQuery(v)) // this will produce request in the same order as input was done
-    switchMap(v => makeQuery(v)) // this will make request only for last input
-  )
-  .subscribe(val => {
-    console.log('request:', val)
-  })
+function getData(value) {
+  cancelRequest.next(); // kill prev request
+
+  someHttpService.makeCall(value)
+    .pipe(
+      takeUntil(cancelRequest)
+    )
+    .subscribe(val => {
+      console.log('request:', val)
+    })
+}
+
+const someHttpService = {
+  makeCall: (value) => {
+    const due = (value === 'John') ? 3000 : 880
+    return of(value).pipe(delay(due))
+  }
+}
 
 // User add some values in "runtime"
-setTimeout(() => userInputSubj.next('John'), 800)
-setTimeout(() => userInputSubj.next('John "Bones"'), 1600)
-setTimeout(() => userInputSubj.next('John "Bones" Johns'), 2400)
-
-
-//Simulate http request here
-
-
-function makeQuery(value) {
-  const due = (value === 'John') ? 3000 : 880
-  return of(value).pipe(delay(due))
-}
+setTimeout(() => getData('John'), 800)
+setTimeout(() => getData('John "Bones"'), 1600)
+setTimeout(() => getData('John "Bones" Johns'), 2400)
