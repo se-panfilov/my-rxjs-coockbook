6f50d5993956a17843ff8f94f9ef4a97b83aac9e
Sergei Panfilov <se-panfilov@ya.ru>
2019-04-05 14:56:18 +0200

    fixes

--- examples/error-catch.js
+++ examples/error-catch.js
@@ -1,18 +1,17 @@
 const {of, Subject, forkJoin, zip, merge, throwError} = require('rxjs')
 const {delay, takeUntil, flatMap, map, switchMap, every, take, tap, catchError} = require('rxjs/operators')
 
-function mainEmmiter() {
+function mainEmiter() {
   return throwError('Main Error')
 }
 
 
-mainEmmiter()
+mainEmiter()
   .pipe(
     catchError(errorResponse => {
       console.log('catch')
       return throwError(errorResponse);
     }),
-    tap(console.log)
   )
   .subscribe(val => {
     console.info('subscr');
