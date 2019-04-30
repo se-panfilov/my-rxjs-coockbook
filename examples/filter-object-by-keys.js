f657f6cde911173320617cd25468efe54c96c739
Sergei Panfilov <se-panfilov@ya.ru>
2019-04-30 12:39:45 +0200

    filter objects by keys case added

---
+++ examples/filter-object-by-keys.js
@@ -0,0 +1,61 @@
+const {of, Subject, forkJoin, zip, merge, combineLatest} = require('rxjs')
+const {delay, takeUntil, flatMap, map, switchMap, every, take, tap, reduce} = require('rxjs/operators')
+
+// Case 1:
+// Filter object by array of keys to exclude (here open letters are allowed, consonant letters are not)
+
+const keysToExclude = ['b', 'c', 'd', 'g']
+
+const source$ = of({
+  a: 'yes',
+  b: 'no',
+  c: 'no',
+  d: 'no',
+  e: 'yes',
+  g: 'no',
+})
+
+source$
+  .pipe(
+    map(obj => omit(keysToExclude)(obj))
+  )
+  .subscribe(console.log)
+
+
+// Case 2:
+// Filter object by array of keys to include(here open letters are allowed, consonant letters are not)
+
+const allowedKeys = ['a', 'e']
+
+source$
+  .pipe(
+    map(obj => omitBut(allowedKeys)(obj))
+  )
+  .subscribe(console.log)
+
+////// Helper functions ////// ////// ////// ////// ////// ////// ////// ////// ////// ////// 
+
+function omit(keysToExcludeArr) {
+  return (o) =>
+    Object.keys(o).reduce((acc, key) => {
+      if (keysToExcludeArr.includes(key)) {
+        return acc;
+      }
+      acc[key] = o[key];
+
+      return acc;
+    }, {});
+}
+
+function omitBut(keysToSaveArr) {
+  return (o) =>
+    Object.keys(o).reduce((acc, key) => {
+      if (!keysToSaveArr.includes(key)) {
+        return acc;
+      }
+      acc[key] = o[key];
+
+      return acc;
+    }, {});
+}
+
